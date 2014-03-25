7aa19d15a8c994691ab40c902ca7e05e35625c16
execgerbil <jo@lionlifecoaching.com>
2014-03-25 12:53:36 -0700

    pm tool initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PmJf::Application.config.secret_key_base = '4909c9a28547dc59be3f7b732c5337bf3e26c0ec948951596fbc3b63206f3da2fdceef9f59cfcb4aa2168d09ffef10d1f3b29fed0a31f67be58cb62dac26053a'
